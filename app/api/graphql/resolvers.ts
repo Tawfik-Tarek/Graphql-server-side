de5300fa57b78299b8418e75c9260dc31e3793f8
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-11-14 14:49:41 +0200

    feat: add GraphQL resolvers and schema for Person, Animal, and bird types

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,3 +1,33 @@
-const resolvers = {}
+const resolvers = {
+  AnimalOrBird: {
+    __resolveType(obj) {
+      if (obj.speed) {
+        return 'Animal'
+      }
+      if (obj.wingspan) {
+        return 'bird'
+      }
+    },
+  },
+  Person: {
+    name: (parent) => parent.name.toUpperCase(),
+  },
+  Query: {
+    hello: () => 'Hello World',
+    people: () => {
+      return [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' },
+      ]
+    },
+    animalOrBird: () => {
+      return [
+        { id: 1, name: 'Cheetah', speed: 75 },
+        { id: 1, name: 'Eagle', wingspan: 2 },
+        { id: 1, name: 'Penguin', wingspan: 1 },
+      ]
+    },
+  },
+}
 
 export default resolvers
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,5 +1,28 @@
 const schema = `#graphql
+  type Person {
+    id: ID!
+    name: String!
+  }
 
+  type Animal {
+    id: ID!
+    name: String!
+    speed: Int!
+  }
+
+  type bird  {
+    id: ID!
+    name: String!
+    wingspan: Int!
+  }
+
+  union AnimalOrBird = Animal | bird
+
+  type Query {
+    hello: String!,
+    people: [Person!]!
+    animalOrBird: [AnimalOrBird]!
+  }
 `
 
 export default schema
